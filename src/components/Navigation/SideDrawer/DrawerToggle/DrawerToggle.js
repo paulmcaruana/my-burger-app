2e1db1751558e3f97b41d34bf958393789ea1b03
Paul Caruana <paul.caruana@leighton.com>
2020-12-07 11:07:02 +0000

    Section 8 completed app

---
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,13 @@
+import { checkPropTypes } from 'prop-types';
+import React from 'react';
+import classes from './DrawerToggle.module.css';
+
+const drawerToggle = (props) => (
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+    </div>
+);
+
+export default drawerToggle;
